ca9681c152e2a69e2579cf6c7738ac0ba809e97a
lfnandoo <devfernandoof@gmail.com>
2020-10-05 21:23:19 -0300

    feat: image

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,27 @@
+import { Container, Grid, Typography } from "@material-ui/core";
 import styles from "./header.module.scss";
 
 function Header() {
-  return <div className={styles.header}>a</div>;
+  return (
+    <div className={styles.header}>
+      <Container fixed>
+        <Grid container justify="center" alignItems="center" spacing={5}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h3" className={styles.header__title}>
+              Calcule o seu IMC
+            </Typography>
+            <Typography>
+              O IMC é o Índice de Massa Corporal, e é utilizado para saber se o
+              peso está de acordo com a altura
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <img src="/medic.png" alt="Médico e paciente" />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default Header;
